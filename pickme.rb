a66fb0a009dc91103bca595f53c70bdf106b2bca
Errin Larsen <elarsen@insightcards.com>
2011-12-17 11:30:18 -0600

    Add configure block to App

--- pickme.rb
+++ pickme.rb
@@ -4,6 +4,12 @@ require "sinatra"
 
 require_relative "./models/picker"
 
+configure do
+  foo = Picker.new(:name => "Foo",
+                   :description => "The player who has foo should go first")
+  set :deck, [foo]
+end
+
 get "/" do
   haml :index
 end
