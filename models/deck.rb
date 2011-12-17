2e91289a5bdd0391b64cc39c7430fbdf5dee5179
Errin Larsen <elarsen@insightcards.com>
2011-12-17 15:21:09 -0600

    Make some specs pass - implement #new_picker

--- models/deck.rb
+++ models/deck.rb
@@ -1,7 +1,20 @@
+require_relative "picker"
+
 class Deck
   attr_reader :pickers
+  attr_writer :picker_maker
 
   def initialize
     @pickers = []
+  end
+
+  def new_picker(*args)
+    picker_maker.call(*args)
+  end
+
+
+  private
+  def picker_maker 
+    @picker_maker ||= Picker.public_method(:new)
   end
 end
