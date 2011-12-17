1db1a7eb95aabc122f262ab1b21274d930917d21
Errin Larsen <elarsen@insightcards.com>
2011-12-17 16:21:30 -0600

    Spec out the Picker's responsibility to insert itself into a deck

--- spec/models/picker_spec.rb
+++ spec/models/picker_spec.rb
@@ -12,9 +12,35 @@ describe Picker do
     @it.description.must_be_nil
   end
 
+  it "should support reading and writing a name" do
+    @it.name = "Foo"
+    @it.name.must_equal "Foo"
+  end
+
+  it "should support reading and writing a description" do
+    @it.description = "Bar"
+    @it.description.must_equal "Bar"
+  end
+
   it "should support setting attributes in the initializer" do
     it = Picker.new(:name => "Foo", :description => "Foo is Bar")
     it.name.must_equal "Foo"
     it.description.must_equal "Foo is Bar"
+  end
+
+  describe "#insert" do
+    before do
+      @deck = MiniTest::Mock.new
+      @it.deck = @deck
+    end
+
+    after do
+      @deck.verify
+    end
+
+    it "should add the picker to the deck" do
+      @deck.expect :add_picker, nil, [@it]
+      @it.insert
+    end
   end
 end
