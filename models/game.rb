d5029b69cddbb9c8f5a042057a5634ed30bb04e0
Errin Larsen <elarsen@insightcards.com>
2011-12-17 19:52:19 -0600

    Attempt some dependency inject

--- models/game.rb
+++ models/game.rb
@@ -4,8 +4,10 @@ class Game
   attr_reader :deck
   attr_writer :card_maker
 
-  def initialize
-    @deck = []
+  def initialize(deck_type=Array)
+    @deck_type = deck_type
+    @cards = @deck_type.new
+    setup_deck
   end
 
   def new_card(*args)
@@ -15,10 +17,11 @@ class Game
   end
 
   def add_card(card)
-    @deck << card
+    @cards << card
   end
 
   def play
+    setup_deck if @deck.empty?
     return draw_card
   end
 
@@ -28,7 +31,11 @@ class Game
     return @card_maker ||= Card.public_method(:new)
   end
 
+  def setup_deck
+    @deck = @deck_type.new(@cards).shuffle
+  end
+
   def draw_card
-    return @deck.first
+    return @deck.shift
   end
 end
