f8de02a0d9657e092f52476f73349b49038d91d6
Errin Larsen <elarsen@insightcards.com>
2011-12-17 18:42:40 -0600

    Correct App with new Game methods

--- pickme.rb
+++ pickme.rb
@@ -2,10 +2,17 @@ require "rubygems"
 require "bundler/setup"
 require "sinatra"
 
-require_relative "./models/picker"
+require_relative "models/game"
+
+configure do
+  game = Game.new
+  foo = game.new_card(:name => "Foo",
+                      :description => "The player who has foo should go first")
+  foo.include
+  set :game, game
+end
 
 get "/" do
-  @card = Picker.new(:name => "Foo",
-                 :description => "The player who has foo should go first")
+  @card = settings.game.play
   haml :index
 end
