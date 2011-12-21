dc9e4721277e7996e652107fa8e680ec0339d50e
Errin Larsen <elarsen@insightcards.com>
2011-12-21 16:22:35 -0600

    explicitly require haml

--- pickme.rb
+++ pickme.rb
@@ -1,6 +1,7 @@
 require "rubygems"
 require "bundler/setup"
 require "sinatra"
+require "haml"
 
 require_relative "models/game"
 
@@ -8,7 +9,10 @@ configure do
   game = Game.new
   foo = game.new_card(:name => "Foo",
                       :description => "The player who has foo should go first")
+  bar = game.new_card(:name => "Bar",
+                      :description => "The player who has bar should go first")
   foo.include
+  bar.include
   set :game, game
 end
 
