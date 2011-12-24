8f99a449ccd1d08bfa1b878992c3200f4a2016b4
Errin Larsen <elarsen@insightcards.com>
2011-12-24 11:39:38 -0600

    Clean up the picker_card_steps a little bit

--- features/step_definitions/picker_card_steps.rb
+++ features/step_definitions/picker_card_steps.rb
@@ -2,10 +2,11 @@ Given /^the following picker cards exist:$/ do |card_table|
   stacked_deck = []
   class << stacked_deck; def shuffle; dup; end; end
   Capybara.app.settings.deck = PickerDeck.new(stacked_deck)
-  card_table.hashes.each do |card|
+
+  card_table.hashes.each do |new_card|
     Capybara.app.settings.deck.new_card(
-      :name => card[:name],
-      :description => card[:description]
+      :name => new_card[:name],
+      :description => new_card[:description]
     ).add_to_deck
   end
 end
@@ -32,12 +33,6 @@ Then /^the card named "([^"]*)" has the description "([^"]*)"$/ do |name, descri
   within :css, ".picker-card .name" do
     page.must_have_content name
   end
-  within :css, ".picker-card .description" do
-    page.must_have_content description
-  end
-end
-
-Then /^I should see the picker card description:$/ do |description|
   within :css, ".picker-card .description" do
     page.must_have_content description
   end
