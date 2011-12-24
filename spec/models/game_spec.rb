6e86ae7bb0e1872d56a8aa3e61f5e31df9f75ebe
Errin Larsen <elarsen@insightcards.com>
2011-12-24 10:37:09 -0600

    Change spec for #play to #draw_a_card

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -24,14 +24,14 @@ describe Game do
 
     it "should accept an attribute hash on behalf of the card maker" do
       card_maker = MiniTest::Mock.new
-      card_maker.expect(:call, @new_card, [{ :foo => 42, :bar => "baz" }])
+      card_maker.expect(:call, @new_card, [{ :name => "Foo", :description => "bar" }])
       @it.card_maker = card_maker
-      @it.new_card(:foo => 42, :bar => "baz")
+      @it.new_card(:name => "Foo", :description => "bar")
       card_maker.verify
     end
   end
 
-  describe "#play" do
+  describe "#draw_a_card" do
     before do
       @cards = []
       test_deck = []
@@ -45,17 +45,17 @@ describe Game do
     end
 
     it "should return the first card in the deck" do
-      @it.play.must_equal @cards.first
+      @it.draw_a_card.must_equal @cards.first
     end
 
     it "should return the second card in the deck when called twice" do
-      @it.play
-      @it.play.must_equal @cards[1]
+      @it.draw_a_card
+      @it.draw_a_card.must_equal @cards[1]
     end
 
     it "should reset the deck when all cards have been drawn" do
-      @cards.size.times { @it.play }
-      @it.play.must_equal @cards.first
+      @cards.size.times { @it.draw_a_card }
+      @it.draw_a_card.must_equal @cards.first
     end
   end
 end
