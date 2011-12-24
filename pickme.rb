183870d21c5e624371dd8db62d74c778fec06b24
Errin Larsen <elarsen@insightcards.com>
2011-12-24 11:57:18 -0600

    Implement a cleaner solution to the temporary lack of persistance

--- pickme.rb
+++ pickme.rb
@@ -4,35 +4,12 @@ require "sinatra"
 require "haml"
 
 require_relative "models/picker_deck"
+# temporary solution for the lack of persistance
+require_relative "lib/static_cards_creator"
 
 configure do
   deck = PickerDeck.new
-  deck.new_card(
-    :name => "Freshen up",
-    :description => "The player who most recently bathed should go first"
-  ).add_to_deck
-  deck.new_card(
-    :name => "To the left",
-    :description => "The player left of the pickme player should go first"
-  ).add_to_deck
-  deck.new_card(
-    :name => "Cake day",
-    :description => "The player with the next birthday should go first"
-  ).add_to_deck
-  deck.new_card(
-    :name => "Geezer",
-    :description => "The oldest player should go first"
-  ).add_to_deck
-  deck.new_card(
-    :name => "Concise correspondance",
-    :description => "The player with the shortest email address should go first"
-  ).add_to_deck
-
-  # Wouldn't it be cool if we could do this:
-  # deck = PickerDeck.new do
-  #   card "Foo", "The player who most recently bathed should go first"
-  #   card "Bar", "The player left of the pickme player should go first"
-  # end
+  StaticCardsCreator.new(deck).create
 
   set :deck, deck
 end
